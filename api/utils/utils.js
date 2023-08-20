5e08fbbf1d8057a0ebcfc0da6fe49d7c82dde3de
Z6FFK <ffk@kmd.dk>
2023-08-20 13:17:31 +0200

    Added utils

---
+++ api/utils/utils.js
@@ -0,0 +1,9 @@
+const ENDPOINTS = {
+    STRAVA_TOKEN_ENDPOINT: 'https://www.strava.com/oauth/token',
+}
+
+const GRANT_TYPES = {
+    REFRESH_TOKEN: 'refresh_token',
+}
+
+module.exports = {ENDPOINTS, GRANT_TYPES};
